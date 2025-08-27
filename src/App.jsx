ded71e028f612d5e70e324a6265b76fd9cc3e1cf
darvesh soni <darveshsoni1@gmail.com>
2025-08-27 19:51:44 +0530

    chore: add comment for future home page route implementation

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-         
+          {/* Home page ka route hum agle step mein banayenge */}
         </Routes>
       </main>
     </div>
